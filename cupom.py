a1ea15af4d8130ef993a235c45d10c64e1bbe73f
Nii Miyo <kagnanii.miyo@gmail.com>
2020-09-21 17:25:21 -0300

    teste passando

--- cupom.py
+++ cupom.py
@@ -11,6 +11,38 @@ observacao = "Loja 1317 (PDP)"
 cnpj = "42.591.651/0797-34"
 inscricao_estadual = "244.898.500.113"
 
+
 def dados_loja():
+    global numero
+
     # Implemente aqui
-    return "" 
+    if nome_loja == "":
+        raise Exception("O campo nome da loja é obrigatório")
+
+    if logradouro == "":
+        raise Exception('O campo logradouro do endereço é obrigatório')
+
+    if numero == 0:
+        numero = "s/n"
+
+    if municipio == "":
+        raise Exception('O campo município do endereço é obrigatório')
+
+    if estado == "":
+        raise Exception('O campo estado do endereço é obrigatório')
+
+    if cnpj == "":
+        raise Exception('O campo CNPJ da loja é obrigatório')
+
+    if inscricao_estadual == "":
+        raise Exception('O campo inscrição estadual da loja é obrigatório')
+
+    output = f"{nome_loja}\n"
+    output += f"{logradouro}, {numero} {complemento}\n"
+    output += f"{bairro} - {municipio} - {estado}\n"
+    output += f"CEP:{cep} Tel {telefone}\n"
+    output += f"{observacao}\n"
+    output += f"CNPJ: {cnpj}\n"
+    output += f"IE: {inscricao_estadual}\n"
+
+    return output
